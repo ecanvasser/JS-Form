50b89aa07fd3b546fc11e9772b75c03fe68a2401
ecanvasser <eddiecanvassere9@gmail.com>
2022-11-30 20:33:47 -0800

    Add initial email input functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './style.css'
+
+const Form = () => {
+
+    const emailValidate = () => {
+        const emailInp = document.getElementById('email');
+        
+        document.getElementById('email').addEventListener('input', function() {
+            if (emailInp.validity.valid) {
+                document.getElementById('emailError').textContent = '';
+                emailInp.className = '';
+            } else if (!emailInp.validity.valid) {
+                document.getElementById('emailError').textContent = 'Please enter valid email';
+                emailInp.className = 'error';
+            } else {
+                document.getElementById('emailError').textContent = '';
+                emailInp.className = '';
+            }
+        })
+    }
+
+    return {
+        emailValidate
+    }
+
+}
+
+var form = Form()
+form.emailValidate();
